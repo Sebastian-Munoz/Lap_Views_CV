a41b427542acdadf8c191bbc8577bd1a0f6afafd
Sebastian-munoz <sebastianemunoz@outlook.com>
2020-10-29 21:40:40 -0500

    Add: Ejercicio 5-10

---
+++ app/src/main/java/co/edu/unipiloto/lab_views/Ejercicio_8.java
@@ -0,0 +1,14 @@
+package co.edu.unipiloto.lab_views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ejercicio_8 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio_8);
+    }
+}
